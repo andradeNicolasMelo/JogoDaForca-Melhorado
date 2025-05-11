4f180f665a54269bba8cfafe99c1540d7011a21b
Andrade <andrade.nicolas.melo@outlook.com>
2025-05-10 22:34:04 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaForca-Melhorado.ConsoleApp1/JogoDaForca.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogoDaForca_Refotorado.ConsoleApp1
+{
+    public class JogoDaForca
+    {
+
+    }
+
+    public class PalavraSecreta
+    {
+        //------------ Entrada de dados ----------------
+
+        public string opcaoEscolhida;
+
+        public PalavraSecreta(string temp)
+        {
+            opcaoEscolhida = temp;
+        }
+        //--------------------------------------------
+
+        public string palavraSecreta = "";
+        public string categoria = ""; //categoria escolhida. Apenas tem a função de informar a categoria para o usuario
+        public void DefinicaoPalavraSecreta()
+        {
+
+            do
+            {
+                if (opcaoEscolhida == "1")
+                {
+                    string[] frutas = { "ABACATE", "ABACAXI", "ACEROLA", "ACAI", "ARACA", "ABACATE", "BACABA", "BACURI", "BANANA", "CAJA", "CAJU", "CARAMBOLA", "CUPUACU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MACA", "MANGABA", "MANGA", "MARACUJA", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA" };
+
+                    Random random = new Random();
+                    int indice = random.Next(frutas.Length);
+
+                    palavraSecreta = frutas[indice];
+                    categoria = "Frutas";
+
+                    break;
+                }
+
+                else if (opcaoEscolhida == "2")
+                {
+                    string[] animais = { "CACHORRO", "GATO", "ELEFANTE", "LEAO", "TIGRE", "ZEBRA", "GIRAFA", "MACACO", "RINOCERONTE", "HIPOPOTAMO", "COBRA", "JACARE", "ARARA", "TUCANO", "PANTERA", "LOBO", "URSO", "CAMELO", "GOLFINHO", "BALEIA", "TARTARUGA", "PINGUIM", "CORUJA", "FOCA", "LEOPARDO", "AVESTRUZ", "GALO", "SAPO", "CARANGUEIJO", "BORBOLETA" };
+
+                    Random random = new Random();
+                    int indice = random.Next(animais.Length);
+
+                    palavraSecreta = animais[indice];
+                    categoria = "Animais";
+
+                    break;
+                }
+
+                else if (opcaoEscolhida == "3")
+                {
+                    string[] paises = { "BRASIL", "ARGENTINA", "CANADA", "CHILE", "COLOMBIA", "CUBA", "EQUADOR", "ESTADOS_UNIDOS", "FRANÇA", "ALEMANHA", "INDIA", "ITALIA", "JAPAO", "MÉXICO", "PORTUGAL", "RUSSIA", "ESPANHA", "SUECIA", "SUICA", "CHINA", "AFRICA_DO_SUL", "AUSTRÁLIA", "BELGICA", "DINAMARCA", "EGITO", "FILIPINAS", "GRECIA", "HOLANDA", "IRLANDA", "NORUEGA" };
+
+                    Random random = new Random();
+                    int indice = random.Next(paises.Length);
+
+                    palavraSecreta = paises[indice];
+                    categoria = "Países";
+
+                    break;
+                }
+
+                else
+                {
+                    Console.Write("Escolha 1, 2 ou 3: ");
+                    opcaoEscolhida = Console.ReadLine()!;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ JogoDaForca-Melhorado.ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace jogoDaForca_Refotorado.ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine("Jogo da forca");
+            Console.WriteLine("========================================");
+            Console.WriteLine("1 - Frutas");
+            Console.WriteLine("2 - Animais");
+            Console.WriteLine("3 - Paises");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.Write("Escolha a categoria da palavra secreta: ");
+            string opcaoEscolhida = Console.ReadLine()!;
+
+            //---------------------------------------------
+            PalavraSecreta palavra = new PalavraSecreta(opcaoEscolhida);
+            palavra.DefinicaoPalavraSecreta();
+
+            Console.WriteLine();
+            Console.WriteLine($"Categoria: {palavra.categoria}");
+            Console.WriteLine($"Palavra Secreta: {palavra.palavraSecreta}");
+            Console.WriteLine();
+            //---------------------------------------------
+        }
+    }
+}
